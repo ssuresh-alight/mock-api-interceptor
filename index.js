fb54f89cf1707fb1f261671f5cd489d65742bb65
Sebin Suresh <sebin.suresh@alight.com>
2024-02-13 14:17:02 -0600

    Update server log message to include dynamic port

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ app.get("**", (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port ${PORT}`);
 });
